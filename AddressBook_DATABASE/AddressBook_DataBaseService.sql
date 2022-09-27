ead2eed83b67c769dc04ba21fc4eddb7f03a842b
“preetimca2016” <preetimca2016@gmail.com>
2022-09-27 18:10:06 +0530

    [Preeti]: Create Table of AddressBook

--- AddressBook_DATABASE/AddressBook_DataBaseService.sql
+++ AddressBook_DATABASE/AddressBook_DataBaseService.sql
@@ -3,4 +3,15 @@
 create database AddressBook_ServiceDB
 
 use AddressBook_ServiceDB
-
+--------------------UC2-Create Table----------------------------------------
+CREATE TABLE AddressBookDB(
+EmployeeId int Identity(1,1) primary key,
+FirstName varChar(200), 
+LastName varChar (200),
+Address VarChar(200),
+City varChar(10),
+State varChar(20),
+ZipCode int,
+Phone bigint,
+Email varChar(200)
+);
