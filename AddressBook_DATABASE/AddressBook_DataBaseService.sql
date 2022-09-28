0a100555557342328a7241c64b42937e83376b7a
“preetimca2016” <preetimca2016@gmail.com>
2022-09-28 23:56:36 +0530

    [Preeti]Add: Data insert in Table query.

--- AddressBook_DATABASE/AddressBook_DataBaseService.sql
+++ AddressBook_DATABASE/AddressBook_DataBaseService.sql
@@ -6,12 +6,19 @@ use AddressBook_ServiceDB
 --------------------UC2-Create Table----------------------------------------
 CREATE TABLE AddressBookDB(
 EmployeeId int Identity(1,1) primary key,
-FirstName varChar(200), 
-LastName varChar (200),
-Address VarChar(200),
+FirstName varChar(30), 
+LastName varChar(30),
+Address VarChar(40),
 City varChar(10),
 State varChar(20),
 ZipCode int,
 Phone bigint,
 Email varChar(200)
 );
+------------------------------UC-3-Insert /Add new contacts--------------------------------
+SELECT * From AddressBookDB
+INSERT INTO AddressBookDB;
+VALUES  ('Komal','Yadav''Basai Enclave','GGN', 'Haryan', 122001, 123124324, 'kOMAL@gmail.com')
+		('Radhika','Gandhi','Shiv colony','Mumbai','Marashtra',5655632,6767987865,'radhika  @gmail.com')
+
+
